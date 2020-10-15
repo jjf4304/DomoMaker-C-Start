d69b61264e8863081866d7a64c6e96ac410d2944
jjf4304 <54686120+jjf4304@users.noreply.github.com>
2020-10-15 17:49:08 -0400

    fixed typo

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -15,68 +15,64 @@ const logout = (req, res) => {
 };
 
 const login = (request, response) => {
-    const req = request;
-    const res = response;  
+  const req = request;
+  const res = response;
 
-    //force csat to strings to cover some security flaws
-    const username = `${req.body.username}`;
-    const password = `${req.body.pass}`;
+  // force csat to strings to cover some security flaws
+  const username = `${req.body.username}`;
+  const password = `${req.body.pass}`;
 
-    if(!username||!password){
-        return res.status(400).json({error: 'GRAWAWR! All fields are required.'});
+  if (!username || !password) {
+    return res.status(400).json({ error: 'GRAWAWR! All fields are required.' });
+  }
+
+  return Account.AccountModel.authenticate(username, password, (err, account) => {
+    if (err || !account) {
+      return res.status(400).json({ error: 'Wrong username or password' });
     }
 
-    return Account.AccountModel.authenticate(username, password, (err, account) =>{
-        if(err||!account){
-            return res.status(400).json({error: 'Wrong username or password'});
-        }
-
-        return res.json({redirect: '/maker'});
-    })
+    return res.json({ redirect: '/maker' });
+  });
 };
 
 const signup = (request, response) => {
-    const req = request;
-    const res = response;
+  const req = request;
+  const res = response;
+  // cast to string to cover up security flaws
+  req.body.username = `${req.body.username}`;
+  req.body.pass = `${req.body.pass}`;
+  req.body.pass2 = `${req.body.pass2}`;
 
-    //cast to string to cover up security flaws
-    req.body.username = `${req.body.username}`;
-    req.body.pass = `${req.body.pass}`;
-    req.body.pass2 = `${req.body.pass2}`;
+  if (!req.body.username || !req.body.pass || !req.body.pass2) {
+    return res.status(400).json({ error: 'GRAWAWR! All fields are required.' });
+  }
 
-    if(!req.body.username || !req.body.pass || !req.body.pass2){
-        return res.status(400).json({error: 'GRAWAWR! All fields are required.'});
-    }
+  if (req.body.pass !== req.body.pass2) {
+    return res.status(400).json({ error: 'AWRGRAWR! Passwords do not match.' });
+  }
 
-    if (req.body.pass !== req.body.pass2){
-        return res.status(400).json({error: 'AWRGRAWR! Passwords do not match.'});
-    }
+  return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
+    const accountData = {
+      username: req.body.username,
+      salt,
+      password: hash,
+    };
 
-    return Account.AccountModel.generateHash(req.body.pass, (salt, hash)=>{
-        const accountData = {
-            username: req.body.username,
-            salt,
-            password: hash,
-        };
+    const newAccount = new Account.AccountModel(accountData);
 
-        const newAccount = new Account.AccountModel(accountData);
+    const savePromise = newAccount.save();
+    savePromise.then(() => res.json({ redirect: '/maker' }));
 
-        const savePromise = newAccount.save();
+    savePromise.catch((err) => {
+      console.log(err);
 
-        savePromise.then(()=> res.json({redirect: '/maker'}));
+      if (err.code === 11000) {
+        return res.status(400).json({ error: 'Username already in use.' });
+      }
 
-        savePromise.catch((err) =>{
-            console.log(err);
-
-            if(err.code === 11000){
-                return res.status(400).json({error: 'Username already in use.'});
-            }
-            
-            return res.status(400).json({error: 'An error occurred.'});
-
-        });
+      return res.status(400).json({ error: 'An error occurred.' });
     });
-
+  });
 };
 
 module.exports.loginPage = loginPage;
