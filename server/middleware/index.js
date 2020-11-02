e130e093bbfd315f6a69c7612e7f8ae3e0536f71
jjf4304 <54686120+jjf4304@users.noreply.github.com>
2020-11-01 19:25:19 -0500

    test on middleware

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -26,7 +26,7 @@ const bypassSecure = (req, res, next) => {
 module.exports.requiresLogin = requiresLogin;
 module.exports.requiresLogout = requiresLogout;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
   module.exports.requiresSecure = requiresSecure;
 } else {
   module.exports.requiresSecure = bypassSecure;
